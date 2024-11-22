23f828dc2541b2aa6b8b9e4711be97855dd8aaad
Lucky Elliot <elliotlucky99@gmail.com>
2024-11-22 20:59:25 +0100

    feat: refactored code and fixed build error

--- components/custom-components/MapViewBox.tsx
+++ components/custom-components/MapViewBox.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef } from 'react'
 import Image from 'next/image'
 import { PlaceType } from '@/app/utils/types'
 import CompanyDetail from './CompanyDetail'
